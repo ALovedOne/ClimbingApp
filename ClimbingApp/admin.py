6b1acc8d1273a9d02ec6d7dca22553eed3fcdfc4
mike <mike@from-ring-zero.com>
2015-11-09 18:57:12 -0600

    Added admin

---
+++ ClimbingApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Gym)
+admin.site.register(Wall)
+admin.site.register(Color)
+admin.site.register(Route)
+admin.site.register(Assent)
--- ClimbingApp/models.py
+++ ClimbingApp/models.py
@@ -10,10 +10,13 @@ class Wall(models.Model):
   name = models.CharField(max_length = 32)
   gym  = models.ForeignKey(Gym)
 
+class Color(models.Model):
+  color = models.CharField(max_length = 32)
+
 class Route(models.Model):
   color = models.ForeignKey(Color)
   wall = models.ForeignKey(Wall)
 
 class Assent(models.Model):
   route = models.ForeignKey(Route)
-  date  = models.DateField(auto_now = true, auto_add_new = true)
+  date  = models.DateField(auto_now = True, auto_now_add = True)
