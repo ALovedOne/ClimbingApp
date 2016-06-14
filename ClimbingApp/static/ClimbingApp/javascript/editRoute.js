5d47f21f598edb84aca9f5f3fb559c4be50e5120
mike <mike@from-ring-zero.com>
2016-06-13 21:34:33 -0500

    And the editRoute controller

--- ClimbingApp/static/ClimbingApp/javascript/editRoute.js
+++ ClimbingApp/static/ClimbingApp/javascript/editRoute.js
@@ -1,81 +1,78 @@
-define(['app'],
-function(app) {
+define(['app', 'baseView'],
+function(app, baseView) {
   'use strict';
 
-  var controller = ['$scope', '$state', '$mdDialog', 'GymResource', 'DifficultyResource', 'ColorResource', 'gym', 'wall', 'route',
-  function($scope, $state, $mdDialog, GymResource, DifficultyResource, ColorResource, gym, wall, route) {
-    $scope.route = route;
-    $scope.setDateObj =    date2Object(route.setDate, true);
-    $scope.removeDateObj = date2Object(route.removeDate);
+  var controllerParams = ['$scope', '$state', '$mdDialog', 'GymResource', 'DifficultyResource', 'ColorResource', 'gym', 'wall', 'route'];
+  var controllerFn = function($scope, $state, $mdDialog, GymResource, DifficultyResource, ColorResource, gym, wall, route) {
+    baseView.call(this);
+
+    this.$scope    = $scope;
+    this.$mdDialog = $mdDialog;
+
+    this.DifficultyResource = DifficultyResource;
+    this.ColorResource = ColorResource;
+
+    this.gym   = gym;
+    this.wall  = wall;
+    this.route = route;
+
+    this.setDateObj =    this.date2Object(route.setDate, true);
+    this.removeDateObj = this.date2Object(route.removeDate);
 
     if (route.difficulty) {
-      $scope.difficulties = [route.difficulty];
+      this.difficulties = [route.difficulty];
     }
+
     if (route.color) {
-      $scope.colors =       [route.color];
+      this.colors =       [route.color];
     }
+  };
+  controllerFn.prototype = Object.create(baseView.prototype);
+
     
-    function date2Object(date, defaultToday) {
-      if (date) {
-        return new Date(date);
-      } else if (defaultToday) {
-        return new Date();
-      } else {
-        return null;
-      }
-    }
+  controllerFn.prototype.acceptDialog = function($event) {
+    this.route.wall       = this.wall.resource_uri;
+    this.route.color      = this.route.color.resource_uri;
+    this.route.difficulty = this.route.difficulty.resource_uri;
+    this.route.setDate    = this.object2Date(this.setDateObj);
+    this.route.removeDate = this.object2Date(this.removeDateObj);
+    
+    this.route.$save().then(function(newRoute) {
+      newRoute.color = this.route.color;
+      newRoute.difficulty = this.route.difficulty;
+      this.$mdDialog.hide(newRoute);
+    }.bind(this));
+  }
 
-    function obj2Date(date) {
-      if (date == null) {
-        return null;
-      } else {
-        return date.toISOString().split("T")[0];
-      }
-    }
+  controllerFn.prototype.cancelDialog = function($event) {
+    this.$mdDialog.cancel(false);
+  }
 
-    $scope.acceptDialog = function($event) {
-      route.wall       = wall.resource_uri;
-      route.color      = $scope.route.color.resource_uri;
-      route.difficulty = $scope.route.difficulty.resource_uri;
-      route.setDate    = obj2Date($scope.setDateObj);
-      route.removeDate = obj2Date($scope.removeDateObj);
-      
-      route.$save().then(function(newRoute) {
-        newRoute.color = $scope.route.color;
-        newRoute.difficulty = $scope.route.difficulty;
-        $mdDialog.hide(newRoute);
-      });
+  controllerFn.prototype.loadDifficulties = function() {
+    if (this.difficulties && this.difficulties.length != 1) {
+      return this.difficulties;
+    } else {
+     return this.DifficultyResource.objects.$find().then(function(difficultyList) {
+        this.difficulties = difficultyList.objects;
+      }.bind(this));
     }
+  }
 
-    $scope.cancelDialog = function($event) {
-      $mdDialog.cancel(false);
+  controllerFn.prototype.loadColors = function() {
+    if (this.colors && this.colors.length != 1) {
+      return this.colors;
+    } else {
+      return this.ColorResource.objects.$find().then(function(colorList) {
+        this.colors = colorList.objects;
+      }.bind(this));
     }
+  }
 
-    $scope.loadDifficulties = function() {
-      if ($scope.difficulties && $scope.difficulties.length != 1) {
-        return $scope.difficulties;
-      } else {
-       return DifficultyResource.objects.$find().then(function(difficultyList) {
-          $scope.difficulties = difficultyList.objects;
-        });
-      }
-    }
+  controllerFn.prototype.hexValue = function editRoute$hexValue(color) {
+    return 'rgb(' + color.inner_r + ',' + color.inner_g + ',' + color.inner_b + ')';
+  }
 
-    $scope.loadColors = function() {
-      if ($scope.colors && $scope.colors.length != 1) {
-        return $scope.colors;
-      } else {
-        return ColorResource.objects.$find().then(function(colorList) {
-          $scope.colors = colorList.objects;
-        });
-      }
-    }
-
-    $scope.hexValue = function editRoute$hexValue(color) {
-      return 'rgb(' + color.inner_r + ',' + color.inner_g + ',' + color.inner_b + ')';
-    }
-  }];
-
+  var controller = controllerParams.concat([controllerFn]);
   app.controller('ClimbingAppEditRoute', controller);
   return controller;
 });
