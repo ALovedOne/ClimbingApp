bd57b5171ce12be2b1da3f6d8c2c4fe9b8523de0
mike <mike@from-ring-zero.com>
2016-06-24 01:25:47 -0500

    Moved listWalls to new structure

--- ClimbingApp/static/ClimbingApp/javascript/listWalls.js
+++ ClimbingApp/static/ClimbingApp/javascript/listWalls.js
@@ -1,53 +1,57 @@
-define(['app'],
-function(app) {
+define(['app', 'baseListView'],
+function(app, baseView) {
   'use strict';
 
-  var controller = ['$scope', '$state', '$stateParams', '$mdDialog', 'WallResource', 'gym', 'walls',
-  function($scope, $state, $stateParams, $mdDialog, WallResource, gym, walls) {
-    var gymId = $stateParams.gymId;
-    $scope.wallList = walls.objects;
-    $scope.gym = gym;
+  var controllerParams = ['$scope', '$state', '$stateParams', '$mdDialog', 'gym', 'WallResource'];
+  var controllerFn = function($scope, $state, $stateParams, $mdDialog, gym, WallResource) {
+    baseView.call(this, controllerParams, arguments);
 
-    function editWall($event, wall) {
+    WallResource.objects.$find({gym: gym.id}).then(function(walls) {
+      this.wallList = walls.objects;
+    }.bind(this));
+  }
 
-      return $mdDialog.show({
-        templateUrl: '/static/ClimbingApp/partials/editWall.html',
-        controller: 'ClimbingAppEditWall',
-        controllerAs: 'ctrl',
-        locals: {
-          wall: wall,
-          gym: gym,
-        },
-        targetEvent: $event,
-      });
-    }
+  controllerFn.prototype = Object.create(baseView.prototype);
 
-    $scope.deleteWall = function($event, wall) {
-      $event.originalEvent.cancelBubble = true;
-      var confirm = $mdDialog.confirm()
-        .title("Delete this Wall?")
-        .targetEvent($event)
-        .ok("Delete it")
-        .cancel("Don't do it");
+  controllerFn.prototype.editWallPriv = function ClimbingApp$listWalls$editWall($event, wall) {
+    return this.$mdDialog.show({
+      templateUrl: '/static/ClimbingApp/partials/editWall.html',
+      controller: 'ClimbingAppEditWall',
+      controllerAs: 'ctrl',
+      locals: {
+        wall: wall,
+        gym: this.gym,
+      },
+      targetEvent: $event,
+    });
+  }
 
-      $mdDialog.show(confirm).then(function() {
-        wall.$delete().then(function() {
-          $scope.wallList = _.without($scope.wallList, wall);
-        });
-      });
-    }
+  controllerFn.prototype.deleteWall = function ClimbingApp$listWalls$deleteWall($event, wall) {
+    $event.originalEvent.cancelBubble = true;
+    var confirm = this.$mdDialog.confirm()
+      .title("Delete this Wall?")
+      .targetEvent($event)
+      .ok("Delete it")
+      .cancel("Don't do it");
 
-    $scope.addWall = function($event) {
-      $event.originalEvent.cancelBubble = true;
-      var blankWall = WallResource.objects.$create();
-      blankWall.gym = gym.resource_uri;
+    this.$mdDialog.show(confirm).then(function() {
+      wall.$delete().then(function() {
+        this.wallList = _.without($scope.wallList, wall);
+      }.bind(this));
+    }.bind(this));
+  }
 
-      editWall($event, blankWall).then(function(newWall) {
-        $scope.wallList.push(newWall);
-      });
-    }
-    }];
-    
-    app.controller('ClimbingAppListWalls', controller);
-    return controller;
+  controllerFn.prototype.addWall = function ClimbingApp$listWalls$addWall($event) {
+    $event.originalEvent.cancelBubble = true;
+    var blankWall = this.WallResource.objects.$create();
+    blankWall.gym = this.gym.resource_uri;
+
+    this.editWallPriv($event, blankWall).then(function(newWall) {
+      this.wallList.push(newWall);
+    }.bind(this));
+  }
+  
+  var controller = controllerParams.concat([controllerFn]);
+  app.controller('ClimbingAppListWalls', controller);
+  return controller;
 });
