ce1a75e4deef0fdfba791e1d78b83805e6c18f33
mike <mike@from-ring-zero.com>
2016-09-09 23:11:35 -0500

    Cleaned up urls for debuging

--- conf/urls.py
+++ conf/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import patterns, include, url
-
 from django.conf import settings
-from django.conf.urls.static import static
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -21,4 +17,8 @@ urlpatterns = [
     url(r'^', include('ClimbingApp.urls')), 
 
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+  from django.conf.urls.static import static
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
